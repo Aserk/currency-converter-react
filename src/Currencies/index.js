250d355848d993540350d779bb0fefc8b594900a
Rafał Jabłoński <asserk.codes@gmail.com>
2022-11-25 09:22:48 +0100

    added currencies

---
+++ src/Currencies/index.js
@@ -0,0 +1,17 @@
+export const currencies = [
+    {
+        name: "Euro",
+        short: "EUR",
+        rate: 4.70,
+    },
+    {
+        name: "Dolar amerykański",
+        short: "USD",
+        rate: 4.51,
+    },
+    {
+        name: "Funt brytyjski",
+        short: "GBP",
+        rate: 5.46,
+    },
+];
