bc033a3bd3e9fc37b5d7a1f2cc9aeb3100618c93
Rafał Jabłoński <asserk.codes@gmail.com>
2022-11-25 10:34:33 +0100

    added: select takes option from currencies

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,39 +1,48 @@
 import "./style.css";
+import { currencies } from "../Currencies"
 
 const Form = () => (
-    <form className="form js-form">
-        <fieldset className="form__fieldset">
-          <legend className="form__legend">Kalkulator walut</legend>
-          <p>
-            <label>
-              <span className="form__labelText">
-                Kwota PLN:
-              </span>
-              <input className="form__field js-amount" type="number" min="0" />
-            </label>
-          </p>
-          <p>
-            <label>
-              <span className="form__labelText">
-                Waluta:
-              </span>
-              <select className="form__field js-currency" name="currency">
-                <option value="USD" selected>Dolar</option>
-                <option value="EUR">Euro</option>
-                <option value="GBP">Funt Brytyjski</option>
-              </select>
-            </label>
-          </p>
-          <p>
-            Aktualny kurs <span className="js-selectedCurrency"></span>:
-            <span className="js-exchangeRate"></span>
-          </p>
-          <p>
-            <button className="form__button">Przelicz</button>
-          </p>
-          <p className="js-resultField"></p>
-        </fieldset>
-      </form>
+  <form className="form">
+    <fieldset className="form__fieldset">
+      <legend className="form__legend">Kalkulator walut</legend>
+      <p>
+        <label>
+          <span className="form__labelText">
+            Kwota PLN:
+          </span>
+          <input className="form__field" type="number" min="0" />
+        </label>
+      </p>
+      <p>
+        <label>
+          <span className="form__labelText">
+            Waluta:
+          </span>
+          <select
+            className="form__field"
+            name="currency"
+          >
+            {currencies.map(currency => (
+              <option
+                key={currency.short}
+                value={currency.name}
+              >
+                {currency.name}
+              </option>
+            ))};
+          </select>
+        </label>
+      </p>
+      <p>
+        Aktualny kurs <span className="js-selectedCurrency"></span>:
+        <span className="js-exchangeRate"></span>
+      </p>
+      <p>
+        <button className="form__button">Przelicz</button>
+      </p>
+      <p className="js-resultField"></p>
+    </fieldset>
+  </form>
 );
 
 export default Form;
