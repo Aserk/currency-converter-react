a1b68095c5a94edde43ae4578ece4df27f90bd33
Rafał Jabłoński <asserk.codes@gmail.com>
2022-11-27 21:30:18 +0100

    changed date appearance and updated gif

--- src/Date/index.js
+++ src/Date/index.js
@@ -1,16 +1,29 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./style.css"
 
 const CurrentDate = () => {
-    const [newDate, setNewDate] = useState(new Date());
+    const [date, setDate] = useState(new Date());
 
-    setInterval(() => {
-        setNewDate(newDate => newDate = new Date());
-    }, 1000);
+    const formattedDate = date.toLocaleDateString("pl", {
+        weekday: "long",
+        day: "numeric",
+        month: "long",
+    });
+
+    useEffect(() => {
+        const intervalID = setInterval(() => {
+            setDate(date => date = new Date());
+        }, 1000);
+
+        return () => {
+            clearInterval(intervalID);
+        }
+    }, []);
+
     return (
-        <div className="date">
-            {newDate.toLocaleString()}
-        </div>
+        <span className="date">
+            {`Dzisiaj jest ${formattedDate} ${date.toLocaleTimeString()}`}
+        </span>
     )
 };
 
