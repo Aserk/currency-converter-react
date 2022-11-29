7269e3bb52338cd82e91bab818976852a3449ce3
Rafał Jabłoński <asserk.codes@gmail.com>
2022-11-29 20:28:52 +0100

    changed style for body on #root

--- src/Container/styled.js
+++ src/Container/styled.js
@@ -3,6 +3,8 @@ import styled from "styled-components";
 export const StyledContainer = styled.div`
     flex-basis: 900px;
     padding: 20px;
-    margin: 0 auto;
+    margin: auto 20px;
     color: white;
+    background-color: hsla(156, 7%, 15%, 0.9);
+    display: grid;
 `;
