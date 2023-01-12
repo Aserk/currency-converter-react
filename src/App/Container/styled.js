fcada6494c4112c3aae237b07b8b873229900b8c
Rafał Jabłoński <asserk.codes@gmail.com>
2023-01-12 22:27:01 +0100

    min height setted

--- src/App/Container/styled.js
+++ src/App/Container/styled.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 export const Container = styled.div`
     flex-basis: 800px;
+    min-height: 500px;
     padding: 20px;
     margin: auto 20px;
     color: ${({ theme }) => theme.color.white};
